329baec767034ff313a3af77fd72c759f5037cab
Jade Ryan Blancaflor <131427861+blancaflor480@users.noreply.github.com>
2025-03-28 08:56:01 +0800

    Update

--- dataentry.sql
+++ dataentry.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2025 at 02:48 AM
+-- Generation Time: Mar 28, 2025 at 01:54 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -65,7 +65,32 @@ CREATE TABLE `employees` (
 --
 
 INSERT INTO `employees` (`id`, `firstName`, `middleName`, `lastName`, `employeeNo`, `status`, `position`, `dateHire`, `endDate`, `footSize`, `weight`, `height`, `personalContact`, `personalEmail`, `corporateEmail`, `birthday`, `address`, `startingRate`, `currentMonthlyRate`, `currentDailyRate`, `bdoAccount`, `sssNumber`, `pagIbigNumber`, `philhealthNumber`, `tinNumber`, `joiningContractUrl`, `probationContractUrl`, `regularContractUrl`, `createdAt`, `updatedAt`) VALUES
-(1, 'Jade', 'Leba', 'Blancaflor', '101', 'Regular', 'Managing Director', '2025-03-24', '2025-06-24', '22', '22', '22', '09380438403', 'bryanblancaflor007@gmail.com', 'bryanblancaflor007@gmail.com', '2025-03-24', 'Kaingin', 100.00, 110.00, 100.00, '1111', '1111', '11', '111', '1111', 'https://drive.google.com/uc?export=view&id=15yJjTKBMyOzsL4qx6NBD6Ys3VqF7um0I', 'https://drive.google.com/uc?export=view&id=14NfRlmp1K33qNEna83VIUnbUSW4WNbpJ', 'https://drive.google.com/uc?export=view&id=1n3AztYSYcgrRu9N-PrmSAh9JOKREum0A', '2025-03-24 08:58:05', '2025-03-25 01:40:50');
+(1, 'Jade Ryan', 'Leba', 'Blancaflor', '101', 'Regular', 'Human Resources', '2025-03-27', '2025-04-01', '11', '100', '5\'7', '0938 043 8403', 'bryanblancaflor007@gmail.com', 'blancaflor480@gmail.com', '2002-08-17', 'Kaingin, Tramo Street, Bacoor', 10000.00, 10000.00, 750.00, '1111-1111-1111', '11-1111111-1', '1111-1111-1111', '11-111111111-1', '111-111-111-111', 'https://drive.google.com/uc?export=view&id=19HSUxam8A4pH-UwaPYmp5gIjHiw7cij1', 'https://drive.google.com/uc?export=view&id=1-tf9AFQVte7sNp6iQNLyS1DB1VKrCkUU', 'https://drive.google.com/uc?export=view&id=1io1TmNptPWNHoWm0ZZ_Edu07nTzY-Eso', '2025-03-27 03:12:08', '2025-03-27 03:12:08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_records`
+--
+
+CREATE TABLE `employee_records` (
+  `recordID` int(11) NOT NULL,
+  `employeeNo` varchar(50) DEFAULT NULL,
+  `type` enum('NTE','IR','Memo') NOT NULL,
+  `dateIssued` date NOT NULL,
+  `details` text NOT NULL,
+  `attachment` varchar(255) DEFAULT NULL,
+  `status` enum('Pending','Resolved','Active') DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employee_records`
+--
+
+INSERT INTO `employee_records` (`recordID`, `employeeNo`, `type`, `dateIssued`, `details`, `attachment`, `status`) VALUES
+(1, '101', 'NTE', '2025-03-27', 'Hellow', NULL, 'Resolved'),
+(2, '101', 'IR', '2025-03-27', 'aaaa', 'https://drive.google.com/uc?export=view&id=1yyiiqMYgY6REW_fDpO8iFChhRvwnSfph', 'Resolved'),
+(3, '101', 'IR', '2025-03-27', 'aaaa', 'https://drive.google.com/uc?export=view&id=1c4IRiUP1JDV-Nz49g-lXYjN1kAqNfsNP', 'Resolved');
 
 --
 -- Indexes for dumped tables
@@ -79,6 +104,13 @@ ALTER TABLE `employees`
   ADD UNIQUE KEY `employeeNo` (`employeeNo`);
 
 --
+-- Indexes for table `employee_records`
+--
+ALTER TABLE `employee_records`
+  ADD PRIMARY KEY (`recordID`),
+  ADD KEY `employeeNo` (`employeeNo`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -87,6 +119,22 @@ ALTER TABLE `employees`
 --
 ALTER TABLE `employees`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `employee_records`
+--
+ALTER TABLE `employee_records`
+  MODIFY `recordID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `employee_records`
+--
+ALTER TABLE `employee_records`
+  ADD CONSTRAINT `employee_records_ibfk_1` FOREIGN KEY (`employeeNo`) REFERENCES `employees` (`employeeNo`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
